72973b9f3c696dab32724a416def860c98da9726
Sam Glendenning <sam.glendenning@stfc.ac.uk>
2022-01-24 13:00:05 +0000

    Adding creation time and last update time to IamTotpMfa
    This helps to keep track of changes to the MFA secret object, particularly changes to its associated recovery codes.

--- iam-persistence/src/main/java/it/infn/mw/iam/persistence/model/IamTotpMfa.java
+++ iam-persistence/src/main/java/it/infn/mw/iam/persistence/model/IamTotpMfa.java
@@ -16,6 +16,7 @@
 package it.infn.mw.iam.persistence.model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,6 +30,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "iam_totp_mfa")
@@ -48,6 +51,14 @@ public class IamTotpMfa implements Serializable {
 
   @Column(name = "active", nullable = false)
   private boolean active;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "creation_time", nullable = false)
+  private Date creationTime;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "last_update_time", nullable = false)
+  private Date lastUpdateTime;
 
   @OneToMany(mappedBy = "totpMfa", cascade = CascadeType.ALL, fetch = FetchType.EAGER,
       orphanRemoval = true)
@@ -91,6 +102,31 @@ public class IamTotpMfa implements Serializable {
   public void setActive(final boolean active) {
 
     this.active = active;
+  }
+
+  public Date getCreationTime() {
+
+    return creationTime;
+  }
+
+  public void setCreationTime(final Date creationTime) {
+
+    this.creationTime = creationTime;
+  }
+
+  public Date getLastUpdateTime() {
+
+    return lastUpdateTime;
+  }
+
+  public void setLastUpdateTime(final Date lastUpdateTime) {
+
+    this.lastUpdateTime = lastUpdateTime;
+  }
+
+  public void touch() {
+
+    setLastUpdateTime(new Date());
   }
 
   public Set<IamTotpRecoveryCode> getRecoveryCodes() {
